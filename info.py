6553da54a56233bbcb0a3aee13dd7bd36fac22cd
nothings143-art <rajpathi143@gmail.com>
2025-09-05 19:26:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -84,7 +84,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch
 # ============================================================
 # 🔹 DATABASE
 # ============================================================
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sivanesh:9488128178siva@cluster02.uyxp6bz.mongodb.net/?retryWrites=true&w=majority&appName=cluster02")
 DATABASE_NAME = environ.get('DATABASE_NAME', "MyselfNeon")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'neoncollection')
 
@@ -261,5 +261,6 @@ REACTIONS = [
 # Dont remove Credits
 # Developer Telegram @MyselfNeon
 # Update channel - @NeonFiles
+
 
 
